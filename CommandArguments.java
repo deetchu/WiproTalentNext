a9031cc365859479a2df2817a014979c533bbfab
deetchu <60709232+deetchu@users.noreply.github.com>
2020-05-29 09:36:56 +0530

    Add files via upload

---
+++ CommandArguments.java
@@ -0,0 +1,9 @@
+import java.util.*
+public class CommandArguments
+{
+	public static void main(String[] args)
+	{
+		System.out.println(args[0] + “technologies”);
+		System.out.println(args[1]);
+	}
+}
